140843fa1d6b44a2d909b8fcb36e81c87dc0f618
Mykola Shymechko <m.shymechko@brainence.com>
2022-07-25 10:00:32 +0300

    v39

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ var PlaceHolder = function ($scope, $element, $http, $timeout, $compile) {
           console.log("test2", test1.children)
           console.log("test3", test1.children[0])
           console.log("test4", test1.children[1])
-          console.log("test5", test1.firstChild)
+          console.log("test5", test1.firstChild.ownerDocument.getElementById)
 
 
 
