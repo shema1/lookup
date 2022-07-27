9d53bd0bf0e66dc2669f7400d80f86ba24ba6b35
Mykola Shymechko <m.shymechko@brainence.com>
2022-07-27 14:11:44 +0300

    v126

--- index.js
+++ index.js
@@ -1,6 +1,48 @@
 "use strict";
 
+let postCodeInputNew = `
 
+<input lw-tst="input_postalCode" list="postcodes" type="text" autocomplete="off" ng-disabled="sameAsShipping" tabindex="8" ng-model="address.PostCode" ng-change="changePostSearch()">
+
+<!----><button ng-if="!isBillingAddres" lw-tst="lookUp_postalCode" type="button" ng-click="lookUp($event,'POSTALCODE', address.PostCode);" class="btn"><i class="fa fa-search"></i></button><!---->
+
+<datalist id="postcodes">
+
+	<option ng-repeat="item in postcodes" value="{{item}}">
+
+</datalist>
+
+`;
+
+
+
+const lookupControlNew = `
+
+<div class="control-group">
+
+    <label class="control-label">Lookup:</label>
+
+    <div class="controls controls-row">
+
+        <div class="input-append">
+
+            <input id="lookupAddressesInput" list="lookupAddresses" type="text" autocomplete="off"
+
+                ng-disabled="sameAsShipping || !selectedPostcode" tabindex="-1" ng-model="lookupAddress" ng-change="changeLookupAddress()">
+
+            <datalist id="lookupAddresses">
+
+				<option ng-repeat="item in lookupAddresses" value="{{item.formatted}}">
+
+            </datalist>
+
+        </div>
+
+    </div>
+
+</div>
+
+`;
 var PlaceHolder = function ($scope, $element, $http, $timeout, $compile) {
 
 
@@ -45,11 +87,11 @@ var PlaceHolder = function ($scope, $element, $http, $timeout, $compile) {
                     console.log("elem", elem)
                     elem.empty();
 
-                    elem.append($compile(postCodeInput)(scope));
+                    elem.append($compile(postCodeInputNew)(scope));
 
 
 
-                    $($compile(lookupControl)(scope)).insertAfter(elem[0].parentElement.parentElement);
+                    $($compile(lookupControlNew)(scope)).insertAfter(elem[0].parentElement.parentElement);
 
 
 
