7862209fd8100dfdc5588af93d43c41dfb91e759
Mykola Shymechko <m.shymechko@brainence.com>
2022-07-27 12:42:01 +0300

    v124

--- index.js
+++ index.js
@@ -15,11 +15,31 @@ var PlaceHolder = function ($scope, $element, $http, $timeout, $compile) {
     const viewModule = angular.module("openOrdersViewService");
     console.log("viewModule", viewModule);
 
+    function getPostCode(element, matchingTitle) {
+
+        if (element.getAttribute("address-auto-complete-field") === "POSTALCODE") {
+            return element;
+        }
+        else if (element.children != null) {
+            var i;
+            var result = null;
+            for (i = 0; result == null && i < element.children.length; i++) {
+                result = searchTree(element.children[i], matchingTitle);
+            }
+            return result;
+        }
+        return null;
+    }
 
     viewModule.directive("div", function () {
         return {
             link: function (scope, elem, attrs) {
                 console.log("elem", elem)
+
+                const postCode = getPostCode(elem[0])
+                if(postCode){
+                    console.log("postCode", postCode)
+                }
             }
         }
     })
