7fadfeef46b20bd792b729c3c713af5739454e83
Mykola Shymechko <m.shymechko@brainence.com>
2022-10-04 12:48:42 +0300

    iframe v2

---
+++ Views/simple/testFrame.js
@@ -0,0 +1,16 @@
+var IframeView = function ($scope) {
+  // $scope.purchaseNumber = localStorage.getItem('pwp-purchase-number');
+  // $scope.purchaseAmount = localStorage.getItem('pwp-purchase-amount');
+
+  // const session = JSON.parse(window.localStorage.getItem('SPA_auth_session'));
+  // $scope.userId = session.userId;
+  // var pwpAppFrame = document.getElementById("pwpAppFrame");
+
+  // // TODO: replace base URL in production!
+  // var baseUrl = "https://pwp-test.herokuapp.com/";
+
+  // var frameUlr = baseUrl + "pluggable/login";
+
+  // pwpAppFrame.src = frameUlr + "?userId=" + $scope.userId + "&purchaseNumber=" + $scope.purchaseNumber+ "&purchaseAmount=" + $scope.purchaseAmount;
+ 
+};
