69f7a7f4595e2a262e01b2d202261592dfb19cb6
Mykola Shymechko <m.shymechko@brainence.com>
2022-10-18 10:45:21 +0300

    iframe v8

--- Views/simple/views/Iframe.js
+++ Views/simple/views/Iframe.js
@@ -1,4 +1,9 @@
 var IframeView = function ($scope) {
+  console.log("scope iframe", scope)
+
+  const orderInfo = localStorage.getItem('move_cancel_selected_order_info');
+
+  console.log("test iframe orderInfo", orderInfo);
   // $scope.purchaseNumber = localStorage.getItem('pwp-purchase-number');
   // $scope.purchaseAmount = localStorage.getItem('pwp-purchase-amount');
 
@@ -12,6 +17,6 @@ var IframeView = function ($scope) {
 
   // var frameUlr = baseUrl + "pluggable/login";
 
-  pwpAppFrame.src = "http://localhost:2000/?token=6c324e9a-bf7d-adb4-900d-eb4420a1e02d"
- 
+  pwpAppFrame.src = `http://localhost:2000/?token=6c324e9a-bf7d-adb4-900d-eb4420a1e02d?orderId=${orderInfo.OrderId}`
+
 };
