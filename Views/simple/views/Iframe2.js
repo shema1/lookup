42ef98ef4a7f9a8f67c47ec603996e2051900446
Mykola Shymechko <m.shymechko@brainence.com>
2022-11-01 14:58:03 +0200

    v24

--- Views/simple/views/Iframe2.js
+++ Views/simple/views/Iframe2.js
@@ -23,8 +23,11 @@ var Iframe2View = function ($scope) {
   appFrame.src = `http://localhost:2000/?token=6c324e9a-bf7d-adb4-900d-eb4420a1e02d&orderId=${orderInfo.OrderId}&email=akornytskyi@brainence.com`
 
   window.addEventListener('message', function (e) {
-    const test = e;
-    console.log("eeeeeee", e)
-    appFrame.src = ''
+    console.log("eeeeeee", e);
+    const data = JSON.parse(e.data);
+    console.log("eeeeeee data", data.cancel);
+    // if(data.cancel){
+    //   appFrame.src = ''
+    // }
 });
 };
